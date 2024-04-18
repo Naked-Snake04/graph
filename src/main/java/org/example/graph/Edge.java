a6054a4d7ce02b0d0140d3b1531e27dd9188c025
Naked-Snake04 <joKer041200saV@gmail.com>
2024-04-18 23:52:30 +0300

    Добавление ограничения по диаметру

--- src/main/java/org/example/graph/Edge.java
+++ src/main/java/org/example/graph/Edge.java
@@ -4,16 +4,42 @@ package org.example.graph;
  * Класс рёбер
  */
 class Edge implements Comparable<Edge> {
-    int u, v, w;
+    private int u;
+    private int v;
+    private int w;
     Edge(int u, int v, int w)
     {
-        this.u = u;
-        this.w = w;
-        this.v = v;
+        this.setU(u);
+        this.setW(w);
+        this.setV(v);
     }
     public int compareTo(Edge other)
     {
-        return (this.w - other.w);
+        return (this.getW() - other.getW());
+    }
+
+    public int getU() {
+        return u;
+    }
+
+    public void setU(int u) {
+        this.u = u;
+    }
+
+    public int getV() {
+        return v;
+    }
+
+    public void setV(int v) {
+        this.v = v;
+    }
+
+    public int getW() {
+        return w;
+    }
+
+    public void setW(int w) {
+        this.w = w;
     }
 }
 
--- src/main/java/org/example/graph/Graph.java
+++ src/main/java/org/example/graph/Graph.java
@@ -1,14 +1,16 @@
 package org.example.graph;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Класс Граф
  */
 public class Graph {
-    private final int verticesNum; // количество вершин
-    private final List<Integer>[] adj; // смежные вершины
-    private final List<Edge> edges; // рёбра
+    private int verticesNum; // количество вершин
+    private List<Integer>[] adj; // смежные вершины
+    private List<Edge> edges; // рёбра
+
 
     public Graph(int verticesNum) {
         this.verticesNum = verticesNum;
@@ -52,22 +54,64 @@ public class Graph {
 
     public void reverseDeleteMST(int diameter) {
         Collections.sort(edges);
+        List<Edge> resultEdges = new ArrayList<Edge>();
+        HashSet<Edge> finalEdges = new HashSet<>();
         int mstWeight = 0;
         for (int i = edges.size() - 1; i >= 0; i--) {
 //            System.out.println(i);
-            int u = edges.get(i).u;
-            int v = edges.get(i).v;
+            int u = edges.get(i).getU();
+            int v = edges.get(i).getV();
 
             adj[u].remove((Integer) v);
             adj[v].remove((Integer) u);
-
             if (!isConnected()) {
                 adj[u].add(v);
                 adj[v].add(u);
-                System.out.println("(" + (u + 1) + ", " + (v + 1)
-                        + ")");
-                mstWeight += edges.get(i).w;
+                resultEdges.add(new Edge(u, v, edges.get(i).getW()));
+                System.out.println("(" + u + ", " + v + ")");
+                mstWeight += edges.get(i).getW();
             }
+        }
+        System.out.println(mstWeight);
+        // убираем ребра превышающие диаметр
+        resultEdges.sort(Comparator.comparingInt(Edge::getU));
+        LinkedList<Integer> list = new LinkedList<>();
+        ArrayList<Integer> indexes = new ArrayList<>();
+        for (int i = 0; i < resultEdges.size(); i++) {
+            int u = resultEdges.get(i).getU();
+            list.add(u);
+            indexes.add(resultEdges.indexOf(resultEdges.get(i)));
+            int finalI = i;
+            var ref = new Object() {
+                int v = resultEdges.get(finalI).getV();
+            };
+            list.add(ref.v);
+            for (int j = i + 1; j < resultEdges.size(); j++) {
+                if (resultEdges.get(j).getU() == ref.v) {
+                    list.add(resultEdges.get(j).getV());
+                    indexes.add(resultEdges.indexOf(resultEdges.get(j)));
+                    ref.v = resultEdges.get(j).getV();
+                } else if (resultEdges.get(j).getV() == ref.v) {
+                    list.add(resultEdges.get(j).getU());
+                    indexes.add(resultEdges.indexOf(resultEdges.get(j)));
+                    ref.v = resultEdges.get(j).getU();
+                }
+            }
+            indexes.sort(Collections.reverseOrder());
+            if (list.size() > diameter) {
+                while (list.size() != diameter) {
+                    mstWeight -= resultEdges.get(indexes.getFirst()).getW();
+                    list.remove(list.getLast());
+                    if (indexes.size() > diameter)
+                        indexes.remove(indexes.getFirst());
+                }
+            }
+            indexes.sort(Integer::compareTo);
+            for (Integer index: indexes) {
+                finalEdges.add(resultEdges.get(index));
+            }
+            list.clear();
+            indexes.clear();
         }
         System.out.println(mstWeight);
     }
